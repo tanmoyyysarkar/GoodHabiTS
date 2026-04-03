270030b85a6776e0f082233f90d6768fb59030eb
tanmoyyysarkar <tanmoysarkar.ts1234@gmail.com>
2026-04-04 03:19:41 +0530

    Added: Collapsable cards

--- src/components/Hobbies/HobbyWithMiniHeatMap.tsx
+++ src/components/Hobbies/HobbyWithMiniHeatMap.tsx
@@ -4,14 +4,13 @@ import { Ionicons } from '@expo/vector-icons';
 import { Pressable, Text, View } from 'react-native';
 import { ProgressRing } from './ProgressRing';
 import { HobbyHeatmapItem } from '@/types/HobbyHeatMapItem';
+import { useState } from 'react';
 
 interface HobbyWithMiniHeatMapProps {
   isDark: boolean;
   tokens: ThemeTokens;
   data: HobbyHeatmapItem;
 }
-
-
 
 const HobbyWithMiniHeatMap = ({ isDark, tokens, data }: HobbyWithMiniHeatMapProps) => {
   const progress = (data.timeDoneToday / data.totalTimePerDay) * 100;
@@ -40,10 +39,15 @@ const HobbyWithMiniHeatMap = ({ isDark, tokens, data }: HobbyWithMiniHeatMapProp
 
   const heatMapAligned = [...emptyHeats, ...heatMap];
 
+  const [collapsed, setCollapsed] = useState(false);
+  const handlePress = () => {
+    setCollapsed((prev) => !prev);
+  };
+
   return (
     <View
       className={`${isDark ? 'border-border bg-card-bg' : 'border-border-light bg-card-bg-light'} gap-4 rounded-2xl border p-4`}>
-      <View className="flex-row justify-between">
+      <View className="flex-row items-start justify-between">
         <View className="flex-row gap-4">
           <View
             style={{ backgroundColor: data.color }}
@@ -63,7 +67,7 @@ const HobbyWithMiniHeatMap = ({ isDark, tokens, data }: HobbyWithMiniHeatMapProp
             </Text>
           </View>
         </View>
-        <View className="flex items-center justify-center">
+        <Pressable onPress={handlePress} className="items-center self-start">
           <View
             style={{ backgroundColor: data.color }}
             className="flex h-6 items-center justify-center rounded-full px-2">
@@ -72,59 +76,67 @@ const HobbyWithMiniHeatMap = ({ isDark, tokens, data }: HobbyWithMiniHeatMapProp
               🔥{data.streakCount}d
             </Text>
           </View>
-          <Pressable className="mt-2 flex-1">
-            <Ionicons name="chevron-down-outline" size={24} color={'white'} />
-          </Pressable>
-        </View>
+          <View className="mt-2 h-6 w-6 items-center justify-center">
+            <Ionicons
+              name={collapsed ? 'chevron-down-outline' : 'chevron-up-outline'}
+              size={24}
+              color={'white'}
+            />
+          </View>
+        </Pressable>
       </View>
-      <View className="flex-row items-center justify-between gap-4">
-        <View
-          className={`${isDark ? 'bg-card-bg-elevated' : 'bg-card-bg-elevated-light'} h-2 flex-1 rounded-full`}>
-          <View
-            style={{ backgroundColor: data.color, width: `${progress}%` }}
-            className="h-2 rounded-full"
-          />
-        </View>
-        <Text
-          className={`${isDark ? 'text-text-secondary' : 'text-text-secondary-light'} font-jetbrains-mono-light text-xs`}>
-          {data.timeDoneToday}/{data.totalTimePerDay} min today
-        </Text>
-      </View>
-      <View className="flex-row items-center justify-between pr-6">
-        <View className="gap-2">
-          <View className="flex-row gap-1">
-            {days.map((day) => (
-              <View key={day} className="flex h-6 w-6 items-center justify-center">
+      {collapsed ? (
+        <>
+          <View className="flex-row items-center justify-between gap-4">
+            <View
+              className={`${isDark ? 'bg-card-bg-elevated' : 'bg-card-bg-elevated-light'} h-2 flex-1 rounded-full`}>
+              <View
+                style={{ backgroundColor: data.color, width: `${progress}%` }}
+                className="h-2 rounded-full"
+              />
+            </View>
+            <Text
+              className={`${isDark ? 'text-text-secondary' : 'text-text-secondary-light'} font-jetbrains-mono-light text-xs`}>
+              {data.timeDoneToday}/{data.totalTimePerDay} min today
+            </Text>
+          </View>
+          <View className="flex-row items-center justify-between pr-6">
+            <View className="gap-2">
+              <View className="flex-row gap-1">
+                {days.map((day) => (
+                  <View key={day} className="flex h-6 w-6 items-center justify-center">
+                    <Text
+                      className={`${isDark ? 'text-text-tertiary' : 'text-text-tertiary-light'} font-jetbrains-mono-light text-sm`}>
+                      {day}
+                    </Text>
+                  </View>
+                ))}
+              </View>
+              <View className="w-[192px] flex-row flex-wrap gap-1">{heatMapAligned}</View>
+            </View>
+            <View className="flex items-center justify-between p-1">
+              <ProgressRing
+                progress={75} //TODO: add logic
+                size={90}
+                strokeWidth={15}
+                mainColor={data.color}
+                isDark={isDark}
+              />
+              <View className="mt-2 flex items-center justify-center gap-1">
                 <Text
-                  className={`${isDark ? 'text-text-tertiary' : 'text-text-tertiary-light'} font-jetbrains-mono-light text-sm`}>
-                  {day}
+                  className={`${isDark ? 'text-text-secondary' : 'text-text-secondary-light'} font-jetbrains-mono-semibold text-[10px]`}>
+                  WEEKLY AVG
+                </Text>
+                <Text
+                  className={`${isDark ? 'text-text-primary' : 'text-text-primary-light'} font-jetbrains-mono-bold text-xs`} //TODO: Add login
+                >
+                  42 min
                 </Text>
               </View>
-            ))}
+            </View>
           </View>
-          <View className="w-[192px] flex-row flex-wrap gap-1">{heatMapAligned}</View>
-        </View>
-        <View className="flex items-center justify-between p-1">
-          <ProgressRing
-            progress={75}   //TODO: add logic
-            size={90}
-            strokeWidth={15}
-            mainColor={data.color}
-            isDark={isDark}
-          />
-          <View className="mt-2 flex items-center justify-center gap-1">
-            <Text
-              className={`${isDark ? 'text-text-secondary' : 'text-text-secondary-light'} font-jetbrains-mono-semibold text-[10px]`}>
-              WEEKLY AVG
-            </Text>
-            <Text
-              className={`${isDark ? 'text-text-primary' : 'text-text-primary-light'} font-jetbrains-mono-bold text-xs`}  //TODO: Add login
-              >
-              42 min
-            </Text>
-          </View>
-        </View>
-      </View>
+        </>
+      ) : null}
     </View>
   );
 };
